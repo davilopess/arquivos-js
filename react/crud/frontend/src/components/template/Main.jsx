eeafd27a6ead0ecbf2457c41aaefa27c10906e32
Davi Lopes <davi.lopes.amorim2014@hotmail.com>
2020-07-04 05:09:34 -0300

    crud

---
+++ react/crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,12 @@
+import './Main.css';
+import './Header.css';
+
+import React from 'react'
+
+export default props =>
+    <React.Fragment>
+        <Header />
+        <main className="content">
+            Conteudo
+        </main>
+    </React.Fragment>
