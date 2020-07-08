6f9772430f43783ce5dde2b5fe0c49146cca0c92
Davi Lopes <davi.lopes.amorim2014@hotmail.com>
2020-07-08 19:26:44 -0300

    crud

--- react/crud/frontend/src/components/template/Main.jsx
+++ react/crud/frontend/src/components/template/Main.jsx
@@ -6,7 +6,9 @@ import React from 'react'
 export default props =>
     <React.Fragment>
         <Header {...props} />
-        <main className="content">
-            Conteudo
+        <main className="content container-fluid">
+            <div className="p-3 mp-3">
+                {props.children}
+            </div>
         </main>
     </React.Fragment>
