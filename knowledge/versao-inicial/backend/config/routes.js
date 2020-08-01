f424524d5f846059ebb39938d41178ef9bbcf892
davilopess <davi.lopes.amorim2014@hotmail.com>
2020-08-01 20:23:26 -0300

    articles api

--- knowledge/versao-inicial/backend/config/routes.js
+++ knowledge/versao-inicial/backend/config/routes.js
@@ -9,9 +9,22 @@ module.exports = app => {
     app.route('/categories')
         .get(app.api.category.get)
         .post(app.api.category.save)
+
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
     
     app.route('/categories/:id')
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove)
+    
+    app.route('/articles')
+        .get(app.api.articles.get)
+        .post(app.api.articles.save)
+
+    app.route('/articles/:id')
+        .get(app.api.articles.getById)
+        .put(app.api.articles.save)
+        .delete(app.api.articles.remove)
+
 }
