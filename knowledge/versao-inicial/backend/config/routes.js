c1eaa9bab8009de9478c317a9a24e48fb3ad3920
davilopess <davi.lopes.amorim2014@hotmail.com>
2020-08-04 23:34:55 -0300

    Passport de autenticao

--- knowledge/versao-inicial/backend/config/routes.js
+++ knowledge/versao-inicial/backend/config/routes.js
@@ -4,34 +4,42 @@ module.exports = app => {
     app.post('/validateToken', app.api.auth.validateToken)
 
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
     
     app.route('/users/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.user.save)
 
     app.route('/categories')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.get)
         .post(app.api.category.save)
 
     app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
     
     app.route('/categories/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove)
     
     app.route('/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.articles.get)
         .post(app.api.articles.save)
 
     app.route('/articles/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.articles.getById)
         .put(app.api.articles.save)
         .delete(app.api.articles.remove)
 
     app.route('/categories/:id/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.articles.getByCategory)
 
 
